5287a3f5bf58450e424089c9a32d1aa25a511c59
Gilad elran <106150889+AntiToxics@users.noreply.github.com>
2026-03-01 16:56:50 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -7,9 +7,9 @@ Description : This script performs a TCP SYN scan to identify open ports on a re
 """
 
 
-
 from scapy.all import *
 from scapy.layers.inet import TCP, IP
+import logging
 
 FLAG_SYN = "S"
 FLAG_SYN_ACK = "SA"
@@ -26,21 +26,30 @@ def handle_ping(dst_ip):
     - prints all the available ports
     :return:
     """
+    try:
+        logging.info("Starting to scan")
+        for dport in range(24, 1025):
+            try:
+                pkt = IP(dst=dst_ip) / TCP(dport=dport, sport=SPORT, flags=FLAG_SYN)
+                response = sr1(pkt, timeout=0.5, verbose=0)
+                print(f"Scanning port: {dport}")
+                if response and response.haslayer(TCP):
+                    if response[TCP].flags == FLAG_SYN_ACK:
+                        AVAILABLE_PORTS.append(dport)
+            except Exception as e:
+                logging.error(f"Error scanning port {dport}: {e}")
+                print("Error while scanning")
+                
+            logging.info("Scanning complete")
+        print("-------All of the available ports-------")
+        if AVAILABLE_PORTS:
+            for port in AVAILABLE_PORTS:
+                print(f"Port {port} is open")
+        else:
+            print("No available ports")
+    except Exception as e:
+        logging.critical(f"Critical error during scan: {e}")
 
-    for dport in range(24, 1025):
-        pkt = IP(dst=dst_ip) / TCP(dport=dport, sport=SPORT, flags=FLAG_SYN)
-        response = sr1(pkt, timeout=0.5, verbose=0)
-        print(f"Scanning port: {dport}")
-        if response and response.haslayer(TCP):
-            if response[TCP].flags == FLAG_SYN_ACK:
-                AVAILABLE_PORTS.append(dport)
-
-    print("-------All of the available ports-------")
-    if AVAILABLE_PORTS:
-        for port in AVAILABLE_PORTS:
-            print(f"Port {port} is open")
-    else:
-        print("No available ports")
 
 
 def main():
@@ -48,9 +57,19 @@ def main():
      Main function that gets the wanted IP from the user and calls
      the function handle_ping()
     """
+
     dst_ip = input("Enter the destination IP: ")
+    logging.info(f"About to scan IP: {dst_ip}")
     handle_ping(dst_ip)
+    logging.info("Program ended")
 
 
 if __name__ == "__main__":
+    # Logging setup
+    logging.basicConfig(
+        filename='Server.log',
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+
     main()
